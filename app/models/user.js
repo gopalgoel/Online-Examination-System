fd0713e769c51edfcb4eea53c8b282a13b0139c0
devanshu-ghosh <devanshu0987@gmail.com>
2017-03-09 19:56:33 +0530

    Added /login , /logout , /signup , node-cache

--- app/models/user.js
+++ app/models/user.js
@@ -5,18 +5,62 @@ The request till this point is authenticated and authorized , validated
 */
 
 var path = require('path');
+var config = require(path.join(__dirname,'..','config','config'));
+var response = require(path.join(__dirname,'..','config','response'));
+var cache = require(path.join(__dirname,'..','config','cache'));
 var userModel = require(path.join(__dirname,'userSchema'));
+var jwt = require('jsonwebtoken');
+
 exports.signup = function(req,res){
-    //only DB errors possible . other errors handled already at this point 
-    var newUser = new userModel({
-        username:req.query.username,
-        password:req.query.password,
-        role:req.query.role,
-        email:req.query.email
+    //we dont repeat the username 
+    userModel.findOne({username:req.query.username},function(err,docs){
+        if(err)
+            res.json(response(true,err,"",""));
+        else if(docs != null)
+            res.json(response(true,"Username already Taken","",""));
+        else{
+            var newUser = new userModel({
+                username:req.query.username,
+                password:req.query.password,
+                role:req.query.role,
+                email:req.query.email
+            });
+            newUser.save(function(error){
+                if(error)
+                    res.json(response(true,"Db error","",""));
+                res.json(response(false,"","UserCreated",""));
+            });
+        } 
     });
-    newUser.save(function(error){
-        if(error)
-            res.send("Error in Creating User");
-        res.send("User Created");
+}
+
+exports.login = function(req,res){
+    var username = req.query.username;
+    var password = req.query.password;
+    userModel.findOne({username:username,password:password},function(err,docs){
+        if(err)
+            res.json(response(true,err,"",""));
+        else if(docs == null){
+            res.json(response(true,"UserName-Password Combo dont match","",""));
+        }
+        else{
+            var token = jwt.sign({ username: username , password : password },
+                                 config.secretKey);
+            cache.set(token,"session");
+            mykeys = cache.keys();
+            console.log(mykeys);//debug purposes
+            res.json(response(false,"","Token Sent",token));
+        }
     });
+}
+
+exports.logout = function(req,res){
+    var token = req.headers['authorization'];//custom header
+    var value = cache.get(token);
+    if(value != null){
+        var val = cache.del(token);
+        res.json(response(false,"","Logged Out",""));
+    }  
+    else
+        res.json(response(true,"No such token issued","",""));
 }
