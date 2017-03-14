da7a07df683f50fb0208cb4db3da25c80c7f2dc6
gopalgoel19 <mailgopalhere@gmail.com>
2017-03-14 23:51:06 +0530

    Update schemas

--- app/models/testSchema.js
+++ app/models/testSchema.js
@@ -5,19 +5,18 @@ var testSchema = new mongoose.Schema({
     creationDate:{type: Date, default: Date.now},
     testDate:Date,
     name: {type: String, required: true},
+    description: {type: String, required: true},
+    type: {type: String, enum: ['public', 'premium'], required: true},
     owner:{  // creator of the test
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
+        ref: 'user',
+        required: true
     },
-    moderators:[{ //zero or more moderators possible
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
-    }],
     questions:[{
         question: { type: mongoose.Schema.Types.ObjectId, ref: 'question' },
-        marks: {type: Number, default: 4 },
-        negMarks: {type: Number, default: -1}
     }],
+    marks: {type: Number, default: 4 },
+    negMarks: {type: Number, default: -1}
     timeDuration:Number, //in minutes
     prize:String
 });
