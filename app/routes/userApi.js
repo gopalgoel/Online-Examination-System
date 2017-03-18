9793ea65c161cdc2e74e5a3de4ff9f768d86aa36
gopalgoel19 <mailgopalhere@gmail.com>
2017-03-18 20:17:20 +0530

    Clean up

--- app/routes/userApi.js
+++ app/routes/userApi.js
@@ -1,31 +1,10 @@
-/*
-
-Contains API endpoints to 
-	register user
-	and do other stuff
-
-*/
-
-/*
-
-Always follows AUTHENTICATE , AUTHORIZE , VALIDATE and then to models 
-
-*/
-
 var path = require('path');
 var user = require(path.join(__dirname,'..','controller','user'));
 var test = require(path.join(__dirname,'..','controller','test'));
 var auth = require(path.join(__dirname,'..','auth','authenticate'));
 
 module.exports = function(app){
-
 	app.post('/signup',user.signup);
-
 	app.get('/login',user.login);
-
 	app.post('/logout',auth.authenticateRequest,user.logout);
-
-
-	app.post('/test', auth.authenticateRequest, test.createTest);
-	app.get('/test', auth.authenticateRequest, test.getTest);
 }
