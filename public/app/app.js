8fcbbb9390e26a76a66240693c73b7630da8d572
gopalgoel19 <mailgopalhere@gmail.com>
2017-03-23 12:59:20 +0530

    Separate routes to another file

--- public/app/app.js
+++ public/app/app.js
@@ -1,34 +1,7 @@
-var app = angular.module('main',['ngRoute', 'ngCookies']);
+var app = angular.module('main',['routerRoutes', 'ngCookies']);
 
 app.controller('mainController', function(){
 	
-});
-
-app.config(function($routeProvider, $locationProvider) {
-	$routeProvider
-	.when('/home', {
-		templateUrl : 'views/pages/home.html',
-		controller : 'mainController',
-		controllerAs: 'home'
-	})
-	// route for the about page
-	.when('/login', {
-		templateUrl : 'views/pages/login.html',
-		controller : 'loginController',
-		controllerAs: 'c'
-	})
-
-	// route for the contact page
-	.when('/signup', {
-		templateUrl : 'views/pages/signup.html',
-		controller : 'signupController',
-		controllerAs: 'c'
-	})
-	.when('/logout', {
-		templateUrl : 'views/pages/logout.html',
-		controller : 'logoutController',
-		controllerAs: 'c'
-	});
 });
 	
 app.controller('loginController', ["$http", "$cookies", function($http, $cookies){
--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -1,27 +1,29 @@
 angular.module('routerRoutes', ['ngRoute'])
 
 .config(function($routeProvider, $locationProvider) {
-	$routeProvider.when('/', {
-		template: '<h1>Gopal</h1>'
-	// templateUrl : 'home.html',
-	// controller : 'mainController',
-	// controllerAs: 'home'
+	$routeProvider
+	.when('/home', {
+		templateUrl : 'views/pages/home.html',
+		controller : 'mainController',
+		controllerAs: 'home'
+	})
+	// route for the about page
+	.when('/login', {
+		templateUrl : 'views/pages/login.html',
+		controller : 'loginController',
+		controllerAs: 'c'
+	})
+
+	// route for the contact page
+	.when('/signup', {
+		templateUrl : 'views/pages/signup.html',
+		controller : 'signupController',
+		controllerAs: 'c'
+	})
+	.when('/logout', {
+		templateUrl : 'views/pages/logout.html',
+		controller : 'logoutController',
+		controllerAs: 'c'
 	});
-
-	// // route for the about page
-	// .when('/login', {
-	// templateUrl : 'views/pages/login.html',
-	// // controller : 'loginController',
-	// // controllerAs: 'about'
-	// })
-
-	// // route for the contact page
-	// .when('/signup', {
-	// templateUrl : 'views/pages/signup.html',
-	// // controller : 'signupController',
-	// // controllerAs: 'contact'
-	// });
-
-// set our app up to have pretty URLS
-	$locationProvider.htmlMode(true);
+	$locationProvider.html5Mode(true);
 });
